b364984567317eb6f166c9336780f34251a0c265
ananyakavatekar <106848973+ananyakavatekar@users.noreply.github.com>
2026-09-20 16:01:52 -0500

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # using two pointer instead of hashmap solution ensures that space complexity is O(1) instead of O(n)
+        # left and right index take advantage of nums being sorted
+        left = 0
+        right = len(numbers) - 1
+
+        # since its sorted left num is smaller than right num
+        current_sum = numbers[left] + numbers[right]
+        
+        while (current_sum != target): 
+            if (current_sum > target):
+                # make right num smaller
+                right -= 1
+            if (current_sum < target):
+                # make left num bigger
+                left += 1
+            current_sum = numbers[left] + numbers[right]
+        
+        return [left + 1, right + 1]
+
+        
+
